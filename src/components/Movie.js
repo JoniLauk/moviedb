6ea898d1de98973d8227ad8af466954a5b4bc640
Joni Laukkanen <jonilaukkanen1@gmail.com>
2021-09-03 01:04:46 +0300

    Movie component

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,9 +1,14 @@
 import React from "react";
 
 const Movie = (props) => {
+  const posterPath =
+    "https://image.tmdb.org/t/p/original/" + props.movie.poster_path;
+
   return (
     <div>
-      <h1>{props.movie.original_title}</h1>
+      <h2>{props.movie.original_title}</h2>
+      <div>{props.movie.budget}</div>
+      <img src={posterPath} width="150"></img>
     </div>
   );
 };
