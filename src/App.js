393bd373ba62610baaa22cc9328f173dc98237d4
Joni Laukkanen <jonilaukkanen1@gmail.com>
2021-09-05 00:32:25 +0300

    not using state

--- src/App.js
+++ src/App.js
@@ -1,39 +1,12 @@
 import logo from "./logo.svg";
 import React, { useState, useEffect, props } from "react";
 import Movie from "./components/Movie";
+import Movies from "./components/Movies";
 import axios from "axios";
 import "./App.css";
 
-const App = ({}) => {
-  const key = "cc11a5a309558d55e3a5947bb272b95b";
-
-  const [movie, setMovie] = useState({});
-  const padding = {
-    padding: 5,
-  };
-
-  useEffect(() => {
-    axios
-      .get("https://api.themoviedb.org/3/movie/550?api_key=" + key)
-      .then((response) => {
-        setMovie(response.data);
-        console.log(response.data);
-      });
-  }, []);
-
-  return (
-    <div className="Movies">
-      <div>
-        <Movie movie={movie} />
-      </div>
-      <div>
-        <Movie movie={movie} />
-      </div>
-      <div>
-        <Movie movie={movie} />
-      </div>
-    </div>
-  );
+const App = () => {
+  return <Movies />;
 };
 
 export default App;
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -2,13 +2,11 @@ import React from "react";
 import "./Movie.css";
 
 const Movie = (props) => {
-  const posterPath =
-    "https://image.tmdb.org/t/p/original/" + props.movie.poster_path;
+  const posterPath = "https://image.tmdb.org/t/p/original/";
 
   return (
     <div className="Movie">
-      <h2>{props.movie.original_title}</h2>
-      <img className="img" src={posterPath}></img>
+      <h2>{props.movie.title}</h2>
     </div>
   );
 };
